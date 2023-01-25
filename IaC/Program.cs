c303cd2660c9ed3173af885e454129bde97236b1
Guilherme Braga Magalhães <guilherme.magalhaes@mkth.com.br>
2023-01-25 17:48:15 -0300

    finishing template

--- IaC/Program.cs
+++ IaC/Program.cs
@@ -57,7 +57,9 @@ return await Deployment.RunAsync(() =>
         apt-get install -y git-all
 
         curl -LO 'https://dl.k8s.io/release/v1.26.0/bin/linux/amd64/kubectl/bin/linux/amd64/kubectl'
-        sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+        install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+        mkdir Projects
         ";
 
     // Create the virtual machine.
